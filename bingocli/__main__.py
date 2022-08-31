ea94d9d71060a9044948a10fd14cbbab5e3691ba
Shawn Oster <shawn.oster@gmail.com>
2022-08-31 15:28:18 -0700

    factory pattern

--- bingocli/__main__.py
+++ bingocli/__main__.py
@@ -1,12 +1,22 @@
 import argparse
+
+from .provider import services
+from .consolelogging import ConsoleLoggerBuilder
 from .funky import get_funky
-from .logging import services
 
 def main():
     parser = argparse.ArgumentParser(description='Bingo CLI')
     args = parser.parse_args()
     
     get_funky('uncle at a wedding')
+
+    # register and configure services
+    #
+    # if dev use console, else datadog
+    # how does the factory make this better?
+    # services need to be registered before use
+    services.register_builder('console', ConsoleLoggerBuilder())
+
     logger = services.get('console')
     logger.log('Hello World!')
     
